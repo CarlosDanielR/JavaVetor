6090b5cd2fffc1e51bf20eb138ab05f6bd6636cd
CarlosDanielR <87505941+CarlosDanielR@users.noreply.github.com>
2021-10-29 22:57:24 -0300

    JavaVetor

---
+++ testaVetor.java
@@ -0,0 +1,55 @@
+/*
+2) Escreva um programa que leia uma sequência de N n�meros e calcula quantos n�meros na sequência são maiores que a madia dos N n�meros lidos. A quantidade de n�meros N � informado pelo usu�rio
+*/
+// importa biblioteca Scanner
+import java.util.Scanner; 
+public class testaVetor{
+    public static void main(String[] args) {
+    // DECLARACAO DE VARIAVEIS
+    int N, cont,idx;
+    int vetor[];
+    double soma, media;
+    // declara uma varivael de leitura do tipo 
+    // Scanner
+    Scanner ler = new Scanner(System.in);
+    
+    
+    // ENTRADA DE DADOS
+    // imprime uma mensagem na tela
+    System.out.print("digite o valor de N:");
+    // le informacao digitada no teclado
+    N = ler.nextInt();
+
+    vetor = new int[N];
+
+    // PROCESSAMENTO
+    soma = 0;
+    idx = 0;
+    cont=1;
+    while(cont<=N){ // laco infinito
+      System.out.print("digite o "+cont+"o numero:");
+      vetor[idx] = ler.nextInt();
+      soma = soma + vetor[idx];
+      cont = cont + 1;
+      //idx = idx + 1;
+      idx++;
+    }
+    // ja sabemos a soma, podemos calcular a media
+    media = soma / N;
+
+    // conta quantos numeros sao maiores que a media
+    idx = 0;
+    cont = 0;
+    while(idx < N){
+      if( vetor[idx] > media)
+        cont++; // cont = cont + 1
+      idx++;
+    }
+    // SAIDA
+    System.out.println("soma="+soma);
+    //System.out.println("media="+media);
+    System.out.printf("media=%.1f\n",media);
+
+    System.out.println("qtd numeros maiores que media="+cont);
+  }
+}
